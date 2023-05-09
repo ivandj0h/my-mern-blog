fd97576752b5835a29125b67b8caae06a3eb27d9
ivandjoh <ivandi.djoh@gmail.com>
2023-05-10 00:08:46 +0800

    readme updated

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,15 +1,41 @@
-import Post from '../post/Post'
-import './posts.css'
+import { Link } from "react-router-dom";
+import "./posts.css";
 
-export default function Posts() {
+export default function Post({img}) {
   return (
-    <div className='posts'>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+    <div className="post">
+      <img
+        className="postImg"
+        src="https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+        alt=""
+      />
+      <div className="postInfo">
+        <div className="postCats">
+          <span className="postCat">
+            <Link className="link" to="/posts?cat=Music">
+              Music
+            </Link>
+          </span>
+          <span className="postCat">
+            <Link className="link" to="/posts?cat=Music">
+              Life
+            </Link>
+          </span>
+        </div>
+        <span className="postTitle">
+          <Link to="/post/abc" className="link">
+            Lorem ipsum dolor sit amet
+          </Link>
+        </span>
+        <hr />
+        <span className="postDate">1 hour ago</span>
+      </div>
+      <p className="postDesc">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+        officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+        fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+        atque, exercitationem quibusdam, reiciendis odio laboriosam?
+      </p>
     </div>
-  )
+  );
 }
